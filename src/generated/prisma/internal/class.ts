e825eefb4e456069e8c7f47052f50e5a6fca175c
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-11-18 18:50:41 -0300

    conexion con back

--- src/generated/prisma/internal/class.ts
+++ src/generated/prisma/internal/class.ts
@@ -22,7 +22,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "C:\\Users\\Usuario\\Desktop\\Backend-AlgIII\\src\\generated\\prisma",
+      "value": "C:\\Users\\Juan\\source\\repos\\ForoArriz\\Backend-AlgIII\\Backend-AlgIII\\src\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -37,7 +37,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\Usuario\\Desktop\\Backend-AlgIII\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\Juan\\source\\repos\\ForoArriz\\Backend-AlgIII\\Backend-AlgIII\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../../../prisma",
