df55d688f916cbb958174d3b5d85527ef8a5d012
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-11-19 18:21:09 -0300

    fghgf

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(cookieParser());
